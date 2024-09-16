f13ced6735bfa6c7bab0d3b2f0dfac1f2424903b
vinhtoan2807 <vinhtoanluu88@gmail.com>
2024-09-16 14:58:59 +0700

    Update script

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -1,19 +1,12 @@
 // @ts-check
-const { test, expect } = require('@playwright/test');
+const { test, expect } = require("@playwright/test");
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
-});
-
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
-
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+test("login guru", async ({ page }) => {
+  await page.goto("https://demo.guru99.com/v4/");
+  await page.locator("input[name=uid]").fill("mngr589389");
+  await page.locator("input[name=password]").fill("EgahamE");
+  await page.locator("input[name=btnLogin]").click();
+  await expect(page.locator("td>.heading3")).toHaveText(
+    "Welcome To Manager's Page of Guru99 Bank"
+  );
 });
